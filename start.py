4bd51187a208c61343a87b136588f9e513534169
Wiktoria <wika.041199@gmail.com>
2020-02-26 11:12:24 +0100

    initial commit

---
+++ start.py
@@ -0,0 +1,72 @@
+print("Witaj świecie!")
+a = 'Ala'
+b = 'Marek'
+c = 'Kasia'
+print(a)
+print(b)
+print(c)
+
+#for element in kolekcja:
+    #for element2 in kolekcja2:
+
+dlugi_tekst = """ Ala ma kota
+a kot jest głodny. """
+print(dlugi_tekst)
+
+# def funkcja():
+#     """ dokumentacja """
+
+#print(type(a))
+
+print(2 + 3)
+print(2 - 3)
+print(2 * 3)
+print(2 / 3)
+print(2 // 3)
+print(2 % 3)
+print(2 ** 3)
+
+# print(3.34 + 34.34)
+# def pow(liczba, potega):
+#     pass
+
+
+# import math as m # ok
+# from math import * # tak sobie
+# from math import pi, pow, etc... # ok, ale przeslania
+# m.sin()
+# m.pow()
+# m.mean()
+
+print("Ala ma " + str(4) + " lata")
+print("Ala ma {0} lata".format(4))
+print("Ala ma {2} lata".format(4, 4, "Ala", 3.14))
+wiek = 4
+print(f"Ala ma {wiek} lata")
+
+imie = "ala"
+print(imie.upper())
+print(imie)
+imie = imie.upper()
+print(imie[0])
+
+#listy
+lista = [1, 2, 3, 4, 'Ala']
+lista = list()
+lista = lista.append(1)
+print(lista)
+lista[0] = 5 # dla list mozna podmieniac
+lista3 = lista + lista2
+
+
+#slowniki
+slownik = {"imie": "Krzysztof", "wzrost": 183}
+print(slownik["imie"]) # odwolanie poprzez klucz
+
+print(slownik.keys())
+print(slownik.values())
+print(slownik.items())
+str(1)
+int (1)
+float(1)
+
